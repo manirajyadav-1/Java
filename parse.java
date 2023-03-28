46f38375f664a80e9d836fccc9935148b57a13bc
Maniraj Yadav <122726135+manirajyadav-1@users.noreply.github.com>
2023-03-28 23:51:06 +0530

    Add files via upload

---
+++ parse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+import javax.xml.transform.Source;
+
+class Parse { 
+    public static void main(String[] args) {
+        // String str = "123";
+        // int number = Integer.parseInt(str);
+        // System.out.println(number);
+        int number = 123;
+        String str = Integer.toString(number);
+        System.out.println(str.length());
+    }
+}
---
+++ swap.java
@@ -0,0 +1,14 @@
+class Swap {
+    static void swap(int a,int b){
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a + " " + b);
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        swap(a,b);
+    }
+}
